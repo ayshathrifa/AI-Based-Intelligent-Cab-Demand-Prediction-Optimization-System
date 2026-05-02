db788132b052ca08c3bc936758a94dd57a9b2dc7
Aysha Thrifa <ayshathrifa26@gmail.com>
2026-05-02 08:44:24 +0530

    changed route to html render

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ app.register_blueprint(admin_bp,      url_prefix='/api/admin')
 
 @app.route("/")
 def home():
-    return "APP IS RUNNING"
+    return render_template('index.html')
 
 @app.route('/')
 def index():
